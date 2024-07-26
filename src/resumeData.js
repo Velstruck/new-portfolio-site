8ee03eefa984c2ffd7017608be86ce71c6e22167
Vasudev <79050603+Velstruck@users.noreply.github.com>
2024-07-26 22:49:08 +0530

    Small change

--- src/resumeData.js
+++ src/resumeData.js
@@ -57,7 +57,7 @@ let resumeData = {
       //   "Achievements":"Some Achievements"
       // }
     ],
-    "skillsDescription":"Your skills here",
+    "skillsDescription":"My Skills",
     "skills":[
       {
         "skillname":"HTML & CSS"
