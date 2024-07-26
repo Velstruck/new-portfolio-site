37dbd508e80e8df77e954d92374c892edcacb322
Vasudev <79050603+Velstruck@users.noreply.github.com>
2024-07-26 22:35:37 +0530

    devDependencies error fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ export default class Header extends Component {
       
       <header id="home">
          <nav id="nav-wrap">
-            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-          <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+            <a className="mobile-btn"  title="Show navigation">Show navigation</a>
+          <a className="mobile-btn"  title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                <li><a className="smoothscroll" href="#about">About</a></li>
@@ -40,7 +40,7 @@ export default class Header extends Component {
          </div>
 
          <p className="scrolldown">
-            <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+            <a className="smoothscroll"><i className="icon-down-circle"></i></a>
          </p>
 
       </header>
