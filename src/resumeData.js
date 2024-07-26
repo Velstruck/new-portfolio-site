3691e41d0767338d7bd90de9ddef0a927146914c
Vasudev <79050603+Velstruck@users.noreply.github.com>
2024-07-26 21:52:14 +0530

    Initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,116 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Vasudev",
+    "role": "",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of development and like to learn about new technologies or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/vasudev7/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/velstruck",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/velstruck",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at Dr. Ambedkar Institute of Technology and pursuing my B.E. from here. I am a self taught Developer, currently diving deeper into Machine Learning.",
+    "address":"India",
+    "website":"http://github.com/velstruck",
+    "education":[
+      {
+        "UniversityName":"Dr. Ambedkar Institute of Technology",
+        "specialization":"B.E. in Computer Science ",
+        "MonthOfPassing":"",
+        "YearOfPassing":"2021 - 2025",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"DAV Public School",
+        "specialization":"Class XII (CBSE)",
+        "MonthOfPassing":"",
+        "YearOfPassing":"2021",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"CogifyZ Technologies",
+        "specialization":"Web Development Intern",
+        "MonthOfLeaving":"Jun 2024 - Jul",
+        "YearOfLeaving":"2024",
+        "Achievements":""
+      },
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML & CSS"
+      },
+      {
+        "skillname":"JAVASCRIPT"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"C/C++, Python"
+      },
+      {
+        "skillname":"photoshop"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"AI Resume Parser",
+        "description":"Application",
+        "imgurl":"images/portfolio/res.png",
+        "githubUrl": 'https://github.com/Velstruck/AI-resume-parser'
+      },
+      {
+        "name":"Celebrity Predictor",
+        "description":"Application",
+        "imgurl":"images/portfolio/result1.png",
+        "githubUrl": 'https://github.com/Velstruck/ol-celeb'
+      },
+      {
+        "name":"Farmers Management System",
+        "description":"Web App",  
+        "imgurl":"images/portfolio/farmer.png",
+        "githubUrl": 'https://github.com/Velstruck/Farmer-system-management'
+      },
+      {
+        "name":"Discord Bot",
+        "description":"Currently Building",
+        "imgurl":"images/portfolio/bot.png",
+        "githubUrl": 'https://github.com/Velstruck'
+      }
+    ],
+    "testimonials":[
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // },
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // }
+    ]
+  }
+  
+  export default resumeData
