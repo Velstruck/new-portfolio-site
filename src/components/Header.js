71634c4a168b6d6b837dd3fd99dfb017a2571aec
Vasudev <79050603+Velstruck@users.noreply.github.com>
2024-07-26 22:42:18 +0530

    errorlogged

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,9 @@ export default class Header extends Component {
       <React.Fragment>
         <header id="home">
           <nav id="nav-wrap">
-            <a className="mobile-btn" href="#" title="Show navigation">Show navigation</a>
-            <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+            {/* Updated href attributes */}
+            <a className="mobile-btn" href="#show-nav" title="Show navigation">Show navigation</a>
+            <a className="mobile-btn" href="#hide-nav" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
               <li><a className="smoothscroll" href="#about">About</a></li>
@@ -38,7 +39,8 @@ export default class Header extends Component {
           </div>
 
           <p className="scrolldown">
-            <a className="smoothscroll" href="#"><i className="icon-down-circle"></i></a>
+            {/* Updated href attribute */}
+            <a className="smoothscroll" href="#home"><i className="icon-down-circle"></i></a>
           </p>
 
         </header>
